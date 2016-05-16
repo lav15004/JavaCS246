3ef82d00a6fd434a8db56897a05fcfefa62f4cba
Aaron Lavold <lav15004@byui.edu>
2016-05-16 11:05:51 -0700

    changed text

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello GitHub World");
-	System.out.println("Testing....");
+        System.out.println("Hello GitHub");
+	System.out.println("Testing...");
     }
 }
