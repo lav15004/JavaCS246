f52d82f8ffb29a71ae96398258bcb500c9469bfb
Aaron Lavold <lav15004@byui.edu>
2016-05-16 10:39:05 -0700

    fixed line of output

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello GitHumb World");
+        System.out.println("Hello GitHub World");
 	System.out.println("Testing...");
     }
 }
